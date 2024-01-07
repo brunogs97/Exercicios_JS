ac1316968fe593358e541996bb0b5a662e83fb4c
Bruno Gomes <gomes_bruno97@hotmail.com>
2024-01-07 18:21:23 -0300

    exercicios 3 e 4 concluido

--- BOOLEANS/script.js
+++ BOOLEANS/script.js
@@ -27,3 +27,61 @@ function maiorIdade(idade) {
 
 console.log(maiorIdade(16));
 
+/* 
+ 3 - VALIDAÇÃO DE SENHA
+ * Crie uma função que aceite uma senha como argumento. A senha deve ter no mínimo 8 caracteres
+ e conter pelo menos uma letra maiúscula, uma letra minúsucula e um número. 
+*/
+
+function validarSenha(senha) {
+    // validar se a senha tem pelo menos 8 caracteres
+    if(senha.length < 8) {
+        return false;
+    }
+
+    // verificar se a senha tem pelo menos uma letra maiúscula
+    if(!/[A-Z]/.test(senha)) {
+        return false;
+    }
+
+    // verificar se a senha tem pelo menos uma letra minúscula
+    if (!/[a-z]/.test(senha)) {
+        return false;
+    }
+
+    //verificar se a senha contém pelo menos um número
+    if(!/\d/.test(senha)) {
+        return false;
+    }
+
+    //se todas as condições forem atendidas, senha é válida
+    return true;
+    
+}
+
+console.log(validarSenha("Senha1234"));
+console.log(validarSenha("abc123"));
+console.log(validarSenha("Senha")); 
+console.log(validarSenha("12345678"));
+
+/* 
+ 4 - DIA DE SEMANA
+ * Crie uma função que aceite um número representando o dia da semana (1 para Domingo,
+ 2 para Segunda-feira, etc.) e retorne verdadeiro se for um dia útil (Segunda a Sexta)
+ e falso se for um fim de semana. 
+*/
+
+function diaDaSemana(dia) {
+    if(dia == String && dia > 7) return false;
+    
+    if (dia === 1 || dia === 6) return false;
+
+    if (/[2-5]/.test(dia)) return true;
+}
+
+console.log(diaDaSemana(1));
+console.log(diaDaSemana(2));
+console.log(diaDaSemana(4));
+console.log(diaDaSemana(6));
+
+

d3d828fb40352267dd866bb73b1d068c88a9d3f3
Bruno Gomes <gomes_bruno97@hotmail.com>
2024-01-12 20:08:30 -0300

    Exercicios de objeto Math

---
+++ ESTRUTURAS/script.js
@@ -0,0 +1,15 @@
+//OBJETO MATH
+
+/*
+ 1 - ELEVANDO AO QUADRADO:
+ * Crie uma funcao que receba um número como parâmetro e use o objeto `Math`
+ para elevar esse número ao quadrado 
+*/
+
+const x = 4;
+
+function numberSquared(num) {
+    return Math.sqrt(num);
+}
+
+console.log(numberSquared(x));
